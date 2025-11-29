87b66ad652a8f06c2b0bd03d3aaae2b211b699ed
ReelScriptAII-beep <blueberry78902@gmail.com>
2025-11-29 12:17:29 +0530

    Add App.js file for successful React build

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", fontSize: "20px" }}>
+      <h2>Telegram Mini App Working 🎉</h2>
+      <p>Welcome to your first Telegram Mini App!</p>
+    </div>
+  );
+}
+
+export default App;
