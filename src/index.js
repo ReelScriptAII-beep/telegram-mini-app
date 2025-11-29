fd83499714a4258e58cd4d6c2fe46ce5a211a8d4
ReelScriptAII-beep <blueberry78902@gmail.com>
2025-11-29 12:49:52 +0530

    Integrate Telegram WebApp initialization
    Initialize Telegram WebApp if available and simplify rendering.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// Telegram WebApp init
+if (window.Telegram?.WebApp) {
+    window.Telegram.WebApp.ready();
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
