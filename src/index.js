5cd3cf7f45d1d4fa196943bd09df2f5e10e70c03
ReelScriptAII-beep <blueberry78902@gmail.com>
2025-11-29 14:13:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// Telegram WebApp initialization
-if (window.Telegram?.WebApp) {
-  window.Telegram.WebApp.ready();
-  console.log("Telegram WebApp initialized in index.js");
-}
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
