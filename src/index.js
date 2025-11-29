80dc0486714be23647decf76fcc59e05b5d327d3
ReelScriptAII-beep <blueberry78902@gmail.com>
2025-11-29 13:23:37 +0530

    "Final merge: Telegram WebApp + MainButton + UI preserved"

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// Telegram WebApp init
+// Telegram WebApp initialization
 if (window.Telegram?.WebApp) {
-    window.Telegram.WebApp.ready();
+  window.Telegram.WebApp.ready();
+  console.log("Telegram WebApp initialized in index.js");
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
